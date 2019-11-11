83a6f2494501d34b5b367e390c71b2ae3c2938e9
Anna <anna.ilina.21.2000@gmail.com>
2019-11-11 18:41:14 +0500

    Commit 2

--- src/main/java/chatBot/Launch.java
+++ src/main/java/chatBot/Launch.java
@@ -3,7 +3,7 @@ package chatBot;
 import chatBot.Dialog;
 
 public class Launch {
-    public static void main(String[] args) {
+    public static void launch(String[] args) {
         Dialog.getInfo();
         Dialog.bot();
     }
