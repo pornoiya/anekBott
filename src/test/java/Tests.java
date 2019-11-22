5a2eba928ab1d607ac8763cb94fc3f6c8fa114ab
Anna <anna.ilina.21.2000@gmail.com>
2019-11-22 18:59:38 +0500

    Commit numero2

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -9,33 +9,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Tests{
-    @Test
-    public void testGetRandomNum(){
-        Integer randInt = JavaBot.getRandomNum(1, 5);
-        Assert.assertTrue(randInt <= 5 && randInt >= 1);
-        Assert.assertFalse(randInt > 22);
-    }
-
-    @Test
-    public void testFillingDictionary(){
-        HashMap<Integer, String> dictActual = new HashMap<Integer, String>();
-        HashMap<Integer, String> dictExpected = new HashMap<Integer, String>();
-        dictExpected.put(1, "a");
-        dictExpected.put(2, "b");
-        dictExpected.put(3, "c");
-        ArrayList<String> data = new ArrayList<String>();
-        data.add("1a");
-        data.add("2b");
-        data.add("3c");
-        Assert.assertEquals(JavaBot.fillDictionary(dictActual, data), dictExpected);
-    }
-
-    @Test
-    public void testGetJoke(){
-        String actualJoke = JavaBot.getJoke(false);
-        String expectedJoke = Jokes.jokesBase().get(0).substring(1);
-        Assert.assertTrue(actualJoke.equals(expectedJoke));
-    }
 
     @Test
     public void testJokesClass(){
